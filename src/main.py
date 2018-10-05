9466ad40cf3577526190e8639a8e1c64636da800
gauffret <gurvan.auffret@gmx.fr>
2018-10-05 23:38:29 +0200

    Move file in folders

---
+++ src/main.py
@@ -0,0 +1,37 @@
+import sys, os
+sys.path.insert(0, os.path.dirname(__file__) + "/utils")
+from verif_equation import verif_equation
+
+print ("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0")
+print ("5 - 4 * X - 9.3 * 9 * -X^2 = -X^0")
+print ("5 + X4 - 9.3X^2 = 1 * X^0")
+
+while True:
+    equation = input("\nEntrez une équation: ").upper()
+    if (equation == "q") or (equation == "quit") or (equation == "exit"):
+        break
+    equation = equation.replace(' ', '')
+    if (verif_equation(equation) == False):
+        print ("ERROR: Synthax error")
+        continue
+    print ("Equation is valide")
+
+
+    """
+    # Boucle de verification du formatage de la chaine
+    if verif == "ERROR":
+        run = 0
+        print ("EQUATION PAS VALIDE")
+    elif verif == "OK":
+        print ("EQUATION VALIDE")
+
+    while True:
+        response = raw_input("\nRésoudre une nouvelle équation ? (Y/N): ").lower()
+        if (response == "y") or (response == "yes"):
+            run = 1
+            break
+        if (response == "n") or (response == "q") or (response == "quit") or (response == "non") or (response == "no"):
+            run = 0
+            break
+"""
+print("A la prochaine !")
