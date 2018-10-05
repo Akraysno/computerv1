11f79e0bfb3fb7e44b2decf05ad223d819852e96
gauffret <gurvan.auffret@gmx.fr>
2018-10-05 23:17:08 +0200

    remove useless

--- brackets.py
+++ brackets.py
@@ -1,36 +1,25 @@
 def verif_brackets(string: str, index: int):
     lenStr = len(string)
-    print("Verif brackets", string, index, lenStr)
     i = index
     while i < lenStr:
-    #for i in range(index, lenStr - 1):
-        print("boucle", i, string[i], string[i] == '(')
         if string[i] == '(':
-            print("match (")
             i = i + verif_brackets(string[i+1:lenStr], 0) + 2
             if string[i - 1] != ')':
-                print("match ( FAIL")
                 return 0
             continue
         if string[i] == '{':
-            print("match {")
             i = i + verif_brackets(string[i+1:lenStr], 0) + 2
             if string[i - 1] != '}':
-                print("match { FAIL")
                 return 0
             continue
         if string[i] == '[':
-            print("match [")
             i = i + verif_brackets(string[i+1:lenStr], 0) + 2
             if string[i - 1] != ']':
-                print("match [ FAIL")
                 return 0
             continue
         if string[i] == ')' or string[i] == '}' or string[i] == ']':
-            print("Match closure")
             return i
         i += 1
-    print("return 1")
     return 1
 
 def verif_nb_brackets(string: str):
@@ -57,7 +46,4 @@ def verif_nb_brackets(string: str):
 def brackets(string: str):
     if verif_nb_brackets(string) == False:
         return False
-    print("check number")
-    brackets = verif_brackets(string, 0)
-    print(brackets)
-    return True if brackets == 1 else False
+    return True if verif_brackets(string, 0) == 1 else False
