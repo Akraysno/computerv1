0428a01e7619c09fcce4cae5974b38846c4d0196
gauffret <gurvan.auffret@gmx.fr>
2018-10-05 23:58:02 +0200

    Fix exit program

--- src/main.py
+++ src/main.py
@@ -6,10 +6,13 @@ print ("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0")
 print ("5 - 4 * X - 9.3 * 9 * -X^2 = -X^0")
 print ("5 + X4 - 9.3X^2 = 1 * X^0")
 
-while True:
-    equation = input("\nEntrez une équation: ").upper()
+run = True
+while run == True:
+    equation = input("\nEntrez une équation: ").lower()
+    equation = equation.strip(" ")
     if (equation == "q") or (equation == "quit") or (equation == "exit"):
-        break
+        run = False
+        continue
     equation = equation.replace(' ', '')
     if (verif_equation(equation) == False):
         print ("ERROR: Synthax error")
