2272a5b4527545d77416f18bc607fb546c4df5c3
gauffret <gurvan.auffret@gmx.fr>
2018-10-06 05:29:15 +0200

    Add error if half equation is missing

--- src/utils/verif_equation.py
+++ src/utils/verif_equation.py
@@ -3,14 +3,16 @@ from utils import authorizeChar
 from utils import replaceSigns
 from utils import authorizeCharPosition
 
-# return boolean => true no format error
 def verifEquation(equation: str):
     if equation.count('=') != 1:
         print ("SynthaxError: Mauvais nombre de '='")
         return False
     equation = replaceSigns(equation)
     equation_left = equation[0:equation.find('=')]
-    equation_right = equation[equation.find('=') + 1: len(equation) - 1]
+    equation_right = equation[equation.find('=') + 1: len(equation)]
+    if (len(equation_left) == 0) or (len(equation_right) ==0):
+        print ("SyntaxError: Half equation missing")
+        return False
     if (brackets(equation_left) == False) or (brackets(equation_right) == False):
         print ("SynthaxError: Brackets error")
         return False
