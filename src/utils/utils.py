3ea401b6aab92dbcfd53387ec7e1b593b456d7d0
gauffret <gurvan.auffret@gmx.fr>
2018-10-06 03:42:52 +0200

    Add authorize char in verif equation

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,14 @@
+def authorizeChar(string: str, charList:str):
+    for i in range(0, len(string) - 1):
+        validChar = False
+        for j in range(0, len(charList) -1):
+            if (validChar == False) and string[i] == charList[j]:
+                validChar = True
+                break
+        if validChar == False:
+            return False
+    return True
+
 def replaceSigns(string: str):
     while True:
         equation = equation.replace('-+', '-')
--- src/utils/verif_equation.py
+++ src/utils/verif_equation.py
@@ -1,4 +1,5 @@
 from brackets import brackets
+from utils import authorizeChar
 from utils import replaceSigns
 
 # return boolean => true no format error
@@ -9,5 +10,8 @@ def verifEquation(equation: str):
     equation = replaceSigns(equation)
     if brackets(equation) == False:
         print ("SynthaxError: Brackets error")
+        return False
+    if authorizeChar(equation, "0123456789+-*/^") == False:
+        print ("SynthaxError: Forbidden charactere")
         return False
     return True
