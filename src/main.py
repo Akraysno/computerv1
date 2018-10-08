6be48a8439f3ed8186c7042a434383ecb76aeb16
gauffret <gurvan.auffret@gmx.fr>
2018-10-08 09:29:44 +0200

    Remove useless brackets

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ sys.path.insert(0, os.path.dirname(__file__) + "/utils")
 sys.path.insert(0, os.path.dirname(__file__) + "/class")
 from verif_equation import verifEquation
 from equation import Equation
+from resolve_equation import resolveEquation
 
 print ("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0")
 print ("5 - 4 * X - 9.3 * 9 * -X^2 = -X^0")
--- src/utils/brackets.py
+++ src/utils/brackets.py
@@ -7,41 +7,21 @@ def verifBrackets(string: str, index: int):
             if string[i - 1] != ')':
                 return 0
             continue
-        if string[i] == '{':
-            i = i + verif_brackets(string[i+1:lenStr], 0) + 2
-            if string[i - 1] != '}':
-                return 0
-            continue
-        if string[i] == '[':
-            i = i + verif_brackets(string[i+1:lenStr], 0) + 2
-            if string[i - 1] != ']':
-                return 0
-            continue
-        if string[i] == ')' or string[i] == '}' or string[i] == ']':
+        if string[i] == ')':
             return i
         i += 1
     return 1
 
 def verifNbBrackets(string: str):
-    nb_a = 0
-    nb_c = 0
-    nb_p = 0
+    nb = 0
     i = -1
     lenStr = len(string)
     for i in range(0, lenStr - 1):
-        if string[i] == '[':
-            nb_c += 1
-        if string[i] == ']':
-            nb_c -= 1
-        if string[i] == '{':
-            nb_a += 1
-        if string[i] == '}':
-            nb_a -= 1
         if string[i] == '(':
-            nb_p += 1
+            nb += 1
         if string[i] == ')':
-            nb_p -= 1
-    return True if (nb_a == 0 and nb_c == 0 and nb_p == 0) else False;
+            nb -= 1
+    return True if nb == 0 else False;
 
 def brackets(string: str):
     if verifNbBrackets(string) == False:
