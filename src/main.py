d85d235c878f3a3ccb41da98c3ae63c46231b623
Raysnoth <gauffret@e2r10p23.42.fr>
2018-10-12 20:33:19 +0200

    Update exit program

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, signal
 sys.path.insert(0, os.path.dirname(__file__) + "/utils")
 sys.path.insert(0, os.path.dirname(__file__) + "/class")
 from verif_equation import verifEquation
@@ -11,18 +11,28 @@ print ("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0")
 print ("5 - 4 * X - 9.3 * 9 * -X^2 = -X^0")
 print ("5 + X4 - 9.3X^2 = 1 * X^0")
 
-run = True
+def signal_handler(sig, frame):
+    if (sig == 2):
+        print("\n\nUse 'exit' or 'quit' commands next time !")
+        exit()
 
+def exit():
+    print("A la prochaine !")
+    os._exit(0)
 
-while run == True:
-    equationInput = input("\nEntrez une équation: ").lower()
-    equationInput = equationInput.strip(" ")
-    if (equationInput == "q") or (equationInput == "quit") or (equationInput == "exit"):
-        run = False
-        continue
-    if (equationInput == "t") or (equationInput == "test"):
-        runTests()
-        continue
-    runEquation(equationInput)
-
-print("A la prochaine !")
+try:
+    while True:
+        signal.signal(signal.SIGINT, signal_handler)
+        equationInput = input("\nEntrez une équation: ")
+        equationInput = equationInput.lower().strip(" ")
+        if (equationInput == "q") or (equationInput == "quit") or (equationInput == "exit"):
+            run = False
+            continue
+        elif (equationInput == "t") or (equationInput == "test"):
+            runTests()
+            continue
+        else:
+            runEquation(equationInput)
+except EOFError:
+    print("\nError: EOFError")
+    exit()
