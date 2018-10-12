af6efb8998e6dded11a80a384b6b53e1c72d6f4f
gauffret <gurvan.auffret@gmx.fr>
2018-10-12 22:21:55 +0200

    Fix exit program

--- src/main.py
+++ src/main.py
@@ -1,15 +1,12 @@
 import sys, os, signal
 sys.path.insert(0, os.path.dirname(__file__) + "/utils")
 sys.path.insert(0, os.path.dirname(__file__) + "/class")
-from verif_equation import verifEquation
-from equation import Equation
-from resolve_equation import resolveEquation
 from run_equation import runEquation
 from run_equation import runTests
 
-print ("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0")
-print ("5 - 4 * X - 9.3 * 9 * -X^2 = -X^0")
-print ("5 + X4 - 9.3X^2 = 1 * X^0")
+print("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0")
+print("5 - 4 * X - 9.3 * 9 * -X^2 = -X^0")
+print("5 + X4 - 9.3X^2 = 1 * X^0")
 
 def signal_handler(sig, frame):
     if (sig == 2):
@@ -25,9 +22,9 @@ try:
         signal.signal(signal.SIGINT, signal_handler)
         equationInput = input("\nEntrez une équation: ")
         equationInput = equationInput.lower().strip(" ")
+        print(equationInput)
         if (equationInput == "q") or (equationInput == "quit") or (equationInput == "exit"):
-            run = False
-            continue
+            exit()
         elif (equationInput == "t") or (equationInput == "test"):
             runTests()
             continue
