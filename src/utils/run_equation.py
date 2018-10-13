730247f91d1de2d5963926acb25fb3692374178c
gauffret <gurvan.auffret@gmx.fr>
2018-10-13 08:58:10 +0200

    stqrt to implement rpn to resolve equation

--- src/utils/run_equation.py
+++ src/utils/run_equation.py
@@ -5,9 +5,38 @@ from verif_equation import verifEquation
 from equation import Equation
 from utils import check_for_x
 from utils import replaceSigns
+import operator
+
+ops = { '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv
+}
+#http://andreinc.net/2010/10/05/converting-infix-to-rpn-shunting-yard-algorithm/
+def eval_expression(expression):
+    expression = " 4 2 +"
+    tokens = expression.split()
+    stack = []
+
+    for token in tokens:
+        if token in ops:
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = ops[token](arg1, arg2)
+            stack.append(result)
+        else:
+            stack.append(int(token))
+
+    return stack.pop()
 
 def treat_member(member: str):
+    member = member.replace('*', ' * ')
+    member = member.replace('/', ' / ')
+    member = member.replace('+', ' + ')
+    member = member.replace('-', ' - ')
     print("member : ", member)
+    stack = eval_expression(member)
+    print(stack)
 
 def resolve_equation(equation: str):
     equation_left = equation[0:equation.find('=')]
