2c3727f5f36ac52c1634708060b2f2d3f7222f84
gauffret <gurvan.auffret@gmx.fr>
2019-01-20 15:58:44 +0100

    Generate random equation on test

--- src/utils/run_equation.py
+++ src/utils/run_equation.py
@@ -1,17 +1,48 @@
 from equation import Equation
+from random import random
+from math import floor
 
 def runEquation(equation:str):
     try:
         eq = Equation(equation)
-        print(eq)
+        eq.resolve()
     except ValueError as err:
         print("Error : " + err.args[0])
 
 def runTests():
     try:
-        eq = Equation("3x + 2 + 5x * 2 - 3x^2 = 3x^2 + 2x + 5 * 2 - 3x")
+        eq = Equation(genretateRandomValidatedEquation())
         eq.resolve()
-        print(eq)
     except ValueError as err:
         print("Error : " + err.args[0])
-    
+
+def genretateRandomValidatedEquation():
+    memberLeft = generateRandomValidatedMemberEquation()
+    memberRight = generateRandomValidatedMemberEquation() if floor(random() * 2) == 1 else "0"
+    return memberLeft + " = " + memberRight
+
+def generateRandomValidatedMemberEquation():
+    member = ""
+    if floor(random() * 100) > 25:
+        member += generateRandomNumberAsString(len(member) > 0)
+        member += 'x^2'
+    if floor(random() * 100) > 25:
+        member += generateRandomNumberAsString(len(member) > 0)
+        member += 'x'
+    if floor(random() * 100) > 25:
+        member += generateRandomNumberAsString(len(member) > 0)
+    return member if len(member) > 0 else "0"
+
+def generateRandomNumberAsString(spacesAroundOperator: bool):
+    operators = ['+', '-']#, '*', '/']
+    ope = operators[floor(random() * 2)]
+    numberString = ""
+    number = floor(random() * 10) + 1
+    neg = True if floor(random() * 2) == 1 else False
+    if (ope == '-') and (neg == True):
+        ope = '+'
+    elif (ope == '-') or (neg == True):
+        ope = '-'
+    numberString += " "+ope+" " if spacesAroundOperator == True else '-' if neg == True else ''
+    numberString += str(number)
+    return numberString
